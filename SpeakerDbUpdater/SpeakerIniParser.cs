659c7c578a72cf996f15576295c7a6a38e37bcf0
Miloš Kudělka <milos.kudelka@newtontech.cz>
2019-06-21 12:51:42 +0200

    Added hopefully better encoding detection.

--- SpeakerDbUpdater/SpeakerIniParser.cs
+++ SpeakerDbUpdater/SpeakerIniParser.cs
@@ -21,7 +21,13 @@ namespace SpeakerDbUpdater
                 throw new ArgumentException("File does not exist.", nameof(file));
             }
 
-            var fileContent = File.ReadAllText(file, Encoding.GetEncoding(1250));
+            string encoding;
+            using (var reader = File.OpenRead(file))
+            {
+                encoding = DetectFileEncoding(reader);
+            }
+
+            var fileContent = File.ReadAllText(file, Encoding.GetEncoding(encoding));
 
             return fileContent.Split("[", StringSplitOptions.RemoveEmptyEntries)
                 .Where(s => s.Trim() != "")
@@ -40,6 +46,34 @@ namespace SpeakerDbUpdater
 
                     return (SectionName: sectionName, Keys: keys);
                 }).ToList();
+        }
+
+        // https://stackoverflow.com/a/52227667
+        private static string DetectFileEncoding(Stream fileStream)
+        {
+            var Utf8EncodingVerifier = Encoding.GetEncoding("utf-8", new EncoderExceptionFallback(), new DecoderExceptionFallback());
+            using (var reader = new StreamReader(fileStream, Utf8EncodingVerifier,
+                   detectEncodingFromByteOrderMarks: true, leaveOpen: true, bufferSize: 1024))
+            {
+                string detectedEncoding;
+                try
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                    }
+                    detectedEncoding = reader.CurrentEncoding.BodyName;
+                }
+                catch (Exception)
+                {
+                    // Failed to decode the file using the BOM/UTF8. 
+                    // Assume it's local ANSI
+                    detectedEncoding = "ISO-8859-1";
+                }
+                // Rewind the stream
+                fileStream.Seek(0, SeekOrigin.Begin);
+                return detectedEncoding;
+            }
         }
     }
 
