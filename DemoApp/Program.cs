6adcce715ed7f0a03decbd8a8b6839f4ffbe51e6
Richard Klapáč <richardklapac@protonmail.com>
2020-07-09 10:19:29 +0200

    Made DemoApp actually work as a demo.

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using Beey.Client;
+﻿using Beey.Api.Rest;
+using Beey.Client;
+using Beey.DataExchangeModel.Messaging;
 using Serilog;
 using System;
 using System.Collections.Generic;
@@ -6,6 +8,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
 using TranscriptionCore;
 
 namespace DemoApp
@@ -24,12 +27,102 @@ namespace DemoApp
                 .WriteTo.File("beey.log")
                 .CreateLogger();
 
-            string url = "http://localhost:61497";
+            //command line arguments: DemoApp.exe settings.xml audio.mp4
+            var cmdArgs = Environment.GetCommandLineArgs();
+            if (cmdArgs.Length < 3) {
+                Console.WriteLine("Please specify the settings file and the file to be transcribed in the following format:\nthis.exe settings.xml audio.mp4.");
+                Environment.Exit(1);
+            } else if (!File.Exists(cmdArgs[1])) {
+                Console.WriteLine("Settings file not found. Please make sure that the file exists.");
+                Environment.Exit(1);
+            } else if (!File.Exists(cmdArgs[2])) {
+                Console.WriteLine("Audio file not found. Please make sure that the file exists.");
+                Environment.Exit(1);
+            }
 
-            var beey = new BeeyClient(url);
-            await beey.LoginAsync("milos.kudelka@newtontech.cz", "OVPgod");
-            string mp3Path = @"C:\Users\milos.kudelka\Newtontech\BeeyRepo\DotNetClient\XUnitTests\Files\test01.mp3";
-            string trsxPath = @"C:\Users\milos.kudelka\Newtontech\BeeyRepo\DotNetClient\XUnitTests\Files\test01.trsx";
+            //load settings
+            string audioPath = cmdArgs[2];
+            string url = "";
+            string email = "";
+            string password = "";
+
+            //use xml for scalability
+            XmlReader xmlReader = XmlReader.Create(cmdArgs[1]);
+            while (xmlReader.Read()) {
+                if ((xmlReader.NodeType == XmlNodeType.Element)) {
+                    string caseSwitch = xmlReader.Name;
+                    switch (caseSwitch) {
+                        case "Url":
+                            url = xmlReader.ReadElementContentAsString();
+                            break;
+                        case "Email":
+                            email = xmlReader.ReadElementContentAsString();
+                            break;
+                        case "Password":
+                            password = xmlReader.ReadElementContentAsString();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            var beey = new BeeyClient(url); //api
+
+            ////login
+            Console.WriteLine("Logging in.");
+            await beey.LoginAsync(email, password);
+
+            //create project
+            Console.WriteLine("Creating project.");
+            var project = await beey.CreateProjectAsync("projectname", "projectpath", default);
+
+            //upload file
+            Console.WriteLine("Uploading file.");
+            var fileStream = File.Open(audioPath, FileMode.Open);
+            await beey.UploadStreamAsync(project.Id, "audioname.mp3", fileStream, fileStream.Length, false, default);
+
+            //wait for transcoding
+            Console.WriteLine("Waiting for transcoding to finish, this may take a while.");
+            int retryCount = 5;
+            TryValueResult<ProjectProgress> result;
+            //periodically checks if the server has finished a given process until true
+            while ((result = await beey.GetProjectProgressStateAsync(project.Id, default).TryAsync())
+                && !ProcessState.Finished.HasFlag(result.Value.TranscodingState)
+                && retryCount > 0) 
+            {
+                await Task.Delay(3000);
+                retryCount--;
+            }
+
+            //transcribe file
+            Console.WriteLine("Starting transcription.");
+            await beey.TranscribeProjectAsync(project.Id, "cs-CZ", true, true, true, default);
+
+            //wait for transcription
+            Console.WriteLine("Waiting for transcription to finish, this may take a while.");
+            retryCount = 20;
+            //periodically checks if the server has finished a given process until true
+            while ((result = await beey.GetProjectProgressStateAsync(project.Id, default).TryAsync())
+                && !ProcessState.Finished.HasFlag(result.Value.PPCState)
+                && retryCount > 0) {
+                await Task.Delay(5000);
+                retryCount--;
+            }
+
+            //download trsx file
+            Console.WriteLine("Downloading trsx file.");
+            //original is the trsx which is created from the track
+            //current is the trsx which contains all the editing additions
+            var stream = await beey.DownloadOriginalTrsxAsync(project.Id, default);
+
+            byte[] trsx;
+            using (var ms = new MemoryStream()) {
+                stream!.CopyTo(ms);
+                trsx = ms.ToArray();
+            }
+
+            File.WriteAllBytes("transcribed.trsx", trsx);
         }
     }
 }
