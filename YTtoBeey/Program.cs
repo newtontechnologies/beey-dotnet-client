b58f0038ef51c6c584b527dab0d8d251f4102a02
Ondřej Foltýn <ondrej.foltyn@newtontech.cz>
2020-07-19 15:35:52 +0200

    YTtoBeey 1.2r0 bugs fixed, auto tempfiles cleanup workaround beey issues (404), project auto-delete on trsx fail

--- YTtoBeey/Program.cs
+++ YTtoBeey/Program.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.IO;
-using System.Xml;
-using System.Diagnostics;
-using Beey.Client;
+﻿using Beey.Client;
 using Beey.DataExchangeModel.Messaging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
 
 namespace YTtoBeey
 {
     class Program
     {
+        private static BeeyClient beey;
+        private static Beey.DataExchangeModel.Projects.Project project;
+        
+        private static bool transcribed = false;
+
+        private const string verinfo = "YTtoBeey v1.2r0"; 
+
         static async Task Main(string[] args)
         {
+            CleanTemp();
+            Console.CancelKeyPress += Console_CancelKeyPress;
 
             // Commandline args handling
             string configpath = "Settings.xml";
@@ -22,6 +31,7 @@ namespace YTtoBeey
 
             if (args.Length < 1)
             {
+                Console.WriteLine("[INFO] " + verinfo);
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Usage: YTtoBeey.exe <path to mp3>/<video url> (<transcript.trsx>) (<cs-CZ>) (<Settings.xml>)");
                 Console.ResetColor();
@@ -42,7 +52,6 @@ namespace YTtoBeey
             }
 
             // Connect & login to beey
-            BeeyClient beey;
             try
             {
                 Console.WriteLine("[INFO] Login to beey..");
@@ -65,10 +74,6 @@ namespace YTtoBeey
                 return;
             }
 
-            // Create a project
-            Console.WriteLine("[INFO] Creating a project..");
-            var project = await beey.CreateProjectAsync("Atest_" + DateTime.Now.ToFileTime().ToString(), "A/test");
-
             // Get stream to upload
             Stream upstream;
 
@@ -87,14 +92,19 @@ namespace YTtoBeey
                 Console.WriteLine("[INFO] Downloading video...");
                 proc.WaitForExit();
 
-                if (proc.StandardError.ReadToEnd().Contains("ERROR"))
-                { //if youtube-dl returns error
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("[FATAL] Could not download video. Check the url? Maybe video is age restricted?");
-                    Console.ResetColor();
-                    return;
-                }
+                { 
+                    string stderr = proc.StandardError.ReadToEnd();
 
+                    if (stderr.Contains("ERROR"))
+                    { //if youtube-dl returns error
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[FATAL] Could not download video. Check the url? Maybe video is age restricted?");
+                        Console.ResetColor();
+                        Console.WriteLine("Full exception below:\n--\n"+stderr);
+                        return;
+                    }
+                }
+                
                 try
                 {
                     upstream = new FileStream(tmpfile, FileMode.Open);
@@ -105,6 +115,7 @@ namespace YTtoBeey
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("[FATAL] Could not download video.");
                     Console.ResetColor();
+                    Console.WriteLine("Full exception below:\n--\n" + ex.ToString());
                     return;
                 }
             }
@@ -112,6 +123,10 @@ namespace YTtoBeey
             {
                 upstream = new FileStream(videouri, FileMode.Open);
             }
+
+            // Create a project
+            Console.WriteLine("[INFO] Creating a project..");
+            project = await beey.CreateProjectAsync("YTtoBeey_" + DateTime.Now.ToFileTime().ToString(), "YTtoBeey");
 
             // Upload the stream
             Console.WriteLine("[INFO] Uploading..");
@@ -136,7 +151,7 @@ namespace YTtoBeey
             }
 
             // Wait for transcribing
-            await beey.TranscribeProjectAsync(project.Id,language);
+            await beey.TranscribeProjectAsync(project.Id, language);
 
             Console.Write("\n[INFO] Waiting for transcribing");
             while (
@@ -148,15 +163,93 @@ namespace YTtoBeey
                 await Task.Delay(1000);
             }
 
+            await Task.Delay(3000); //workaround, because beey sometimes say it's got trsx ready but it actually does not have it yet
+
+            transcribed = true;
+
             // Download trsx
             Console.WriteLine("\n[INFO] Downloading trsx..");
 
-            Stream downstream = await beey.DownloadOriginalTrsxAsync(project.Id);
+            Stream downstream = null;
+
+            short maxattemps = 5;
+            for (int i = 1; i < maxattemps + 1; i++) //if download trsx fails, retry 5 times; workaround because beey sometimes has wierd issues
+            {
+                try
+                {
+                    downstream = await beey.DownloadOriginalTrsxAsync(project.Id);
+                    break;
+                }
+                catch (Beey.Api.Rest.HttpException ex)
+                {
+                    if (i < maxattemps)
+                        Console.WriteLine("[ERROR] Could not get TRSX file from Beey! ("+ex.HttpStatusCode+") Attempt " + i + "/" + maxattemps);
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[FATAL] Could not get TRSX file from Beey, check connection/try again.");
+                        Console.ResetColor();
+                        Console.WriteLine("Whole exception below:\n--\n");
+                        Console.WriteLine(ex.ToString());
+                        return;
+                    }   
+                }
+                await Task.Delay(3000);
+            }
+
+
             using (FileStream fs = new FileStream(trsxPath, FileMode.Create))
             {
                 downstream!.CopyTo(fs);
             }
             Console.WriteLine("[INFO] Done!");
+            CleanTemp();
+        }
+
+        /// <summary>
+        /// Ctrl+C pressed, perform a cleanup and exit
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static async void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n[FATAL] Ctrl+C was pressed, exiting...");
+            Console.ResetColor();
+            if(project != null && !transcribed)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[WARN] Project does not seem to be transcribed yet; deleting it..");
+                Console.ResetColor();
+                await beey.DeleteProjectAsync(project.Id);
+            }
+            CleanTemp();
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Cleans up redundant temporary files
+        /// </summary>
+        static void CleanTemp()
+        {
+            string[] tmpfiles = Directory.GetFiles(".", "temp*", SearchOption.TopDirectoryOnly);
+            if (tmpfiles.Length > 0)
+            {
+                Console.WriteLine("[INFO] Found redundant temporary files, cleaning up ...");
+                foreach (string file in tmpfiles)
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[ERROR] Failed to delete temporary file: " + file);
+                        Console.ResetColor();
+                    }
+                }
+            }
         }
 
         /// <summary>
