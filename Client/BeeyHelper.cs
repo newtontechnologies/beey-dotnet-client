ccb66ecbd3bf2a39ea8bbe10bfff90df7c97ca09
Miloš Kudělka <milos.kudelka@newtontech.cz>
2020-10-06 13:18:21 +0200

    Implement clever project transcription in BeeyHelper

--- Client/BeeyHelper.cs
+++ Client/BeeyHelper.cs
@@ -1,9 +1,11 @@
 ﻿using Beey.Client.Logging.LogProviders;
 using Beey.DataExchangeModel.Messaging;
+using Beey.DataExchangeModel.Messaging.Subsystems;
 using Beey.DataExchangeModel.Projects;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading;
@@ -98,34 +100,102 @@ namespace Beey.Client
             }
         }
 
-        public static async Task WaitForTranscribableAsync(BeeyClient beey, int projectId, CancellationToken cancellationToken = default)
+        public delegate void Progress(int percentage);
+        public delegate void MediaIdentified(TimeSpan duration);
+
+        /// <summary>
+        /// Waits until project can be transcribed and calls transcription. Calls callback methods when events happen.
+        /// </summary>
+        /// <param name="beey">Client with logged user.</param>
+        /// <param name="projectId"></param>
+        /// <param name="language"></param>
+        /// <param name="withPpc"></param>
+        /// <param name="withVad"></param>
+        /// <param name="withPunctuation"></param>
+        /// <param name="saveTrsx"></param>
+        /// <param name="onMediaIdentified">With duration. Stream has zero duration.</param>
+        /// <param name="onTranscriptionStarted"></param>
+        /// <param name="onUploadProgress">With percentage of upload.</param>
+        /// <param name="onTranscriptionProgress">With percentage of transcription. When percentage is -1, progress is invalid probably because of discrepance between duration in media file header and real dureation.</param>
+        /// <param name="onConversionFinished"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task TranscribeAsync(BeeyClient beey,
+            int projectId, string language = "cs-CZ",
+            bool withPpc = true, bool withVad = true, bool withPunctuation = true, bool saveTrsx = true,
+            MediaIdentified? onMediaIdentified = null, Action? onTranscriptionStarted = null,
+            Progress? onUploadProgress = null, Progress? onTranscriptionProgress = null,
+            Action? onConversionFinished = null,
+            CancellationToken cancellationToken = default)
         {
-            var messages = await beey.ListenToMessages(projectId, cancellationToken);
+            var cts = new CancellationTokenSource();
+            cancellationToken.Register(() => cts.Cancel());
 
-            var progressState = await beey.GetProjectProgressStateAsync(projectId);
-            if (progressState.TranscodingAudioState == ProcessState.Completed)
-            {
-                return;
-            }
-            else if (progressState.TranscodingAudioState == ProcessState.Failed)
-            {
-                throw new Exception("Transcoding failed, cannot transcribe.");
-            }
+            var messages = (await beey.ListenToMessages(projectId, cts.Token))
+                .Select(s => JsonSerializer.Deserialize<Message>(s, Message.CreateDefaultOptions()));
 
-            await foreach (var strMessage in messages.WithCancellation(cancellationToken))
+            TimeSpan duration = TimeSpan.Zero;
+            try
             {
-                var message = JsonSerializer.Deserialize<Message>(strMessage, Message.CreateDefaultOptions());
-                if (message.Type == MessageType.Failed)
+                await foreach (var message in messages.WithCancellation(cts.Token))
                 {
-                    throw new Exception($"Subsystem {message.Subsystem} failed with reason '{((FailedMessage)message).Reason}'.");
-                }
-                if (message.Subsystem == "TranscodingAudio" && message.Type == MessageType.Completed)
-                {
-                    return;
+                    if (message.Type == MessageType.Failed
+                        && message.Subsystem != "MediaIdentification") // TODO: Remove when backend doesn't send fail when media not in faststart.
+                    {
+                        throw new Exception($"{message.Subsystem} failed with reason '{((FailedMessage)message).Reason}'.");
+                    }
+                    if (message.Subsystem == "MediaIdentification" && message.Type == MessageType.Progress)
+                    {
+                        var data = MediaIdentificationData.From(message);
+                        if (data.Kind == MediaIdentificationData.DurationKind.Duration
+                            || data.Kind == MediaIdentificationData.DurationKind.ApproximateDuration
+                            || data.Kind == MediaIdentificationData.DurationKind.DurationlessStream)
+                        {
+                            duration = data.Duration ?? TimeSpan.Zero;
+                            onMediaIdentified?.Invoke(duration);
+                            await beey.TranscribeProjectAsync(projectId, language, withPpc, withVad, withPunctuation, saveTrsx, cts.Token);
+                            onTranscriptionStarted?.Invoke();
+                        }
+                    }
+                    if (message.Subsystem == "MediaFilePackaging" && message.Type == MessageType.Completed)
+                    {
+                        onConversionFinished?.Invoke();
+                    }
+                    if (message.Subsystem == "TranscriptionTracking" && message.Type == MessageType.Completed)
+                    {
+                        onTranscriptionProgress?.Invoke(100);
+                        break;
+                    }
+                    if (message.Subsystem == "Upload" && message.Type == MessageType.Progress)
+                    {
+                        var data = UploadSubsystemData.From(message);
+                        if (data.Kind == UploadSubsystemData.DataKind.UploadedBytes)
+                        {
+                            onUploadProgress?.Invoke(data.UploadPercentage!.Value);
+                        }
+                    }
+                    if (message.Subsystem == "Upload" && message.Type == MessageType.Completed)
+                    {
+                        onUploadProgress?.Invoke(100);
+                    }
+                    if (duration > TimeSpan.Zero // not stream
+                        && message.Subsystem == "Recognition" && message.Type == MessageType.Progress)
+                    {
+                        var data = RecognitionData.From(message);
+                        if (data.Transcribed.HasValue)
+                        {
+                            int percentage = (int)((data.Transcribed.Value.TotalSeconds * 100) / duration.TotalSeconds);
+                            if (percentage > 100)
+                                percentage = -1;
+                            onTranscriptionProgress?.Invoke(percentage);
+                        }
+                    }
                 }
             }
-
-            throw new Exception("No more messages, but conversion did not finish.");
+            finally
+            {
+                cts.Cancel();
+            }
         }
     }
 }
