1ccfb0a0b9dadd34a0539aa93101cd729223794b
Ladislav Šeps <ladislav.seps@newtontech.cz>
2024-08-13 21:52:37 +0200

    simulate X proxy headers in Beey Apps

--- Beey.Proxy/Extensions.cs
+++ Beey.Proxy/Extensions.cs
@@ -1,16 +1,69 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Extensions.Primitives;
 
 namespace Beey.Proxy;
 
 public static class Extensions
 {
-    public static IApplicationBuilder UseBeeyProxy(this IApplicationBuilder builder)
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="injectProxyHeaders">simulate </param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseBeeyProxy(this IApplicationBuilder builder, bool injectProxyHeaders)
     {
-        return builder.UseMiddleware<BeeyProxyMiddleware>();
+        var bldr = builder.UseMiddleware<BeeyProxyMiddleware>();
+
+        if (injectProxyHeaders)
+        {
+            bldr = bldr.Use((context, next) =>
+            {
+                Uri? url = null;
+
+                if (context.Request.Headers.TryGetValue("Beey-IntegrationUrl", out var value))
+                {
+                    url = new Uri(value!)!;
+
+                    //    var holder = context.RequestServices.GetService<BeeyProxyHolder>();
+                    //context.Request.PathBase = ;
+
+                    if (!context.Request.Headers.TryGetValue(ForwardedHeadersDefaults.XForwardedProtoHeaderName, out var proto) || proto != url.Scheme)
+                        context.Request.Headers.Append(ForwardedHeadersDefaults.XForwardedProtoHeaderName, url.Scheme);
+
+                    if (!context.Request.Headers.TryGetValue(ForwardedHeadersDefaults.XForwardedProtoHeaderName, out var host) || host != url.Authority)
+                        context.Request.Headers.Append(ForwardedHeadersDefaults.XForwardedHostHeaderName, url.Authority);
+
+                    if (!context.Request.Headers.TryGetValue(ForwardedHeadersDefaults.XForwardedPrefixHeaderName, out var prefix) || host != url.AbsolutePath)
+                        context.Request.Headers.Append(ForwardedHeadersDefaults.XForwardedPrefixHeaderName, url.AbsolutePath);
+                }
+                var res = next(context);
+
+                if (url is { } && context.Response.Headers.Location != StringValues.Empty)
+                {
+                    //fixup location to relative path for Beey versions prior 1.6
+
+                    var loc = context.Response.Headers.Location.ToString();
+
+                    if (loc.StartsWith(url.ToString()))
+                    {
+                        context.Response.Headers.Location = loc.Substring(url.ToString().Length);
+                    }
+                    else if (loc.StartsWith(url.AbsolutePath.ToString()))
+                    {
+                        context.Response.Headers.Location = loc.Substring(url.AbsolutePath.ToString().Length);
+                    }
+                }
+
+                return res;
+            });
+        }
+
+        return bldr;
     }
 
     public static IServiceCollection AddBeeyProxy(this IServiceCollection services)
